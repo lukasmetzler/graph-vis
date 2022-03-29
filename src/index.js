027f9b79aa0839773474b20fa9780b2a08d5c9a2
lukasmetzler <lukas.metzler@student.uibk.ac.at>
2022-03-29 16:39:04 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
+<BrowserRouter basename="/"></BrowserRouter>
 ReactDOM.render(
   <React.StrictMode>
     <App />
